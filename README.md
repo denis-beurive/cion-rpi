45562119c5c5b0e17217c3a785240d9ca17462d6
Denis Beurivé <denis.beurive@gmail.com>
2023-09-13 12:13:16 +0200

    first import

---
+++ README.md
@@ -0,0 +1,138 @@
+# CLION with Raspberry PI
+
+This document describes the configuration of a Raspberry Pi for CLION (version `2022.3.2`) full remote development.
+
+It also provides a simple example that shows how to use the [libGpiod library](https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/about/) (with threads).
+
+![](doc/images/elect.png)
+
+## Hardware specification:
+
+Raspberry Pi `3` Model B Rev `1.2` (32 bits).
+
+```bash
+$ cat /sys/firmware/devicetree/base/model
+Raspberry Pi 3 Model B Rev 1.2
+$ uname -m
+armv7l # => 32 bits (see: https://forums.raspberrypi.com/viewtopic.php?t=251721) 
+```
+
+## OS specification
+
+Ubuntu server `23.04`.
+
+```bash
+$ lsb_release -a
+No LSB modules are available.
+Distributor ID: Ubuntu
+Description:    Ubuntu 23.04
+Release:        23.04
+Codename:       lunar
+```
+
+> Please, make sure to activate the _SSH login_ when you generate the OS image (using [Raspberry PI Imager](https://www.raspberrypi.com/software/)).
+> See this document: [How to Set Up a Headless Raspberry Pi, Without Ever Attaching a Monitor](https://www.tomshardware.com/reviews/raspberry-pi-headless-setup-how-to,6028.html).
+
+## Install the compilation environment
+
+We need CMAKE version `3.24.4` and GDB version `12.1` (because we use CLION `2022.3.2`).
+
+### CMAKE
+
+Install CMAKE dependencies:
+
+```bash
+sudo apt install g++
+sudo apt install libssl-dev
+```
+
+Then install CMAKE:
+
+```bash
+cd /tmp
+wget https://github.com/Kitware/CMake/archive/refs/tags/v3.24.4.tar.gz
+tar zxvf v3.24.4.tar.gz
+cd /tmp/CMake-3.24.4
+./bootstrap && gmake && sudo make install
+cd /tmp
+rm -rf /tmp/CMake-3.24.4 v3.24.4.tar.gz
+```
+
+> This installation takes a lot of time...
+
+### GDB
+
+Install GDB dependencies:
+
+```bash
+sudo apt install libgmp-dev
+```
+
+Then install GDB:
+
+```bash
+cd /tmp
+wget https://ftp.gnu.org/gnu/gdb/gdb-12.1.tar.gz
+tar zxvf gdb-12.1.tar.gz
+cd gdb-12.1
+./configure
+make
+sudo make install
+cd /tmp
+rm -rf gdb-12.1 gdb-12.1.tar.gz
+```
+## CLION configuration
+
+Assuming that:
+* the hostname of the Raspberry is `rpi1`.
+* the login is `dev`.
+
+![](doc/images/rpi1-conf1.png)
+
+![](doc/images/rpi1-conf2.png)
+
+## Install the libGpiod library
+
+```bash
+apt install gpiod libgpiod-dev libgpiod-doc
+```
+
+> At this point, you should resynchronise the CLION's copy of the remote file system with the remote file system
+> itself. To do that click on `Tools` => `Resync with Remote Hosts` (see [this document](https://www.jetbrains.com/help/clion/remote-projects-support.html#resync) for details).
+
+Test the installation:
+
+```bash
+echo "#include <gpiod.h>"$'\n'"int main(){return 0;}" | gcc -xc -
+echo "int main(){return 0;}" | gcc -xc -lgpiod -
+echo | gcc -E -Wp,-v - | grep -v "# "
+```
+
+Get the GPIO chip name:
+
+```bash
+$ gpiodetect
+gpiochip0 [pinctrl-bcm2835] (54 lines)
+gpiochip1 [brcmvirt-gpio] (2 lines)
+gpiochip2 [raspberrypi-exp-gpio] (8 lines)
+```
+
+> See [this link](https://www.raspberrypi.com/documentation/computers/processors.html): the Raspberry Pi 3 Model B uses
+> the `BCM2837` chip (not the `BCM2835`). However, the underlying architecture of the BCM2837 is identical to the `BCM2836`.
+> And The underlying architecture of the `BCM2836` is identical to the `BCM2835`.
+
+# Useful commands
+
+```bash
+gpiofind "GPIO17"
+gpioset `gpiofind "GPIO17"`=0
+gpioget  `gpiofind "GPIO17"`
+sudo gpioinfo gpiochip0
+gpioset --mode=time --sec=3 `gpiofind "GPIO17"`=1
+gpioset --mode=time --sec=3 -b `gpiofind "GPIO17"`=1
+```
+
+# Example using libGpiod
+
+See [this code](main.c).
+
---
+++ main.c
@@ -0,0 +1,177 @@
+#include <gpiod.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+#include <pthread.h>
+
+// Get the GPIO chip name:
+//
+//     $ gpiodetect
+//     gpiochip0 [pinctrl-bcm2835] (54 lines)
+//     gpiochip1 [brcmvirt-gpio] (2 lines)
+//     gpiochip2 [raspberrypi-exp-gpio] (8 lines)
+//
+// => The chip name is "gpiochip0".
+
+#define CHIP_NAME "gpiochip0"
+
+// Get the number of the:
+//
+//        $ gpioinfo
+//        ...
+//        line  16:     "GPIO16"       unused   input  active-high
+//        line  17:     "GPIO17"       unused   input  active-high
+//        line  18:     "GPIO18"       unused   input  active-high
+//        ...
+//
+// => The ID for "GPIO16" is 16.
+// => The ID for "GPIO17" is 17.
+
+#define RED_LED_LINE 16
+#define GREEN_LED_LINE 17
+
+#define NUMBER_OF_LED 2 // The green LED and the red LED.
+
+/**
+ * Data passed to the function that implements a thread used to control a LED.
+ */
+
+struct led_args {
+    /** The number of seconds to wait for a LED to change its state. */
+    time_t duration_sec;
+    /** The number of nano seconds to wait for a LED to change its state.
+     *  This value must be in the range [0, 999999999]
+     */
+    long duration_nano_sec;
+    /** The number of changes of state. */
+    long count;
+    /** The (GPIO) line ID that controls the state of the LED. */
+    int line_id;
+    const char *name;
+    /** The (GPIO) chip ID. */
+    struct gpiod_chip *chip;
+};
+
+/**
+ * Print an error message and terminate the program.
+ * @param message The message to print.
+ */
+
+void error(char *message) {
+    fprintf(stderr, "ERROR: %s\n", message);
+    exit(1);
+}
+
+/**
+ * Implement the thread that controls a LED.
+ * @param in_args Pointer to `struct led_args`.
+ */
+
+void* led_thread(void *in_args) {
+    struct led_args   *args = (struct led_args*)in_args;
+    struct gpiod_line *led;
+    struct timespec duration = { args->duration_sec, args->duration_nano_sec };
+    struct timespec remaining;
+    struct timespec remaining_again;
+    struct timespec *d, *r;
+
+    // Open GPIO lines. Set the line as output.
+    led = gpiod_chip_get_line(args->chip, args->line_id);
+    if (NULL == led) {
+        error("cannot get the line");
+    }
+
+    // Open LED lines for output
+    if (-1 == gpiod_line_request_output(led, args->name, 0)) {
+        gpiod_line_release(led);
+        error("cannot request the output");
+    }
+
+    for (long cycle=0; cycle<args->count; cycle++) {
+        int state = (cycle & 0x1) != 0;
+        int done;
+
+        d = &duration;
+        r = &remaining;
+        printf("G [%4ld] Set %s\n", cycle, state ? "up" : "down");
+        if (-1 == gpiod_line_set_value(led, state)) {
+            error("cannot change the value of the output");
+        }
+
+        do {
+            done = 1;
+            if (-1 == nanosleep(d, r)) {
+                switch (errno) {
+                    case EINTR: {
+                        d = r;
+                        r = &remaining_again;
+                        done = 0;
+                    }; break;
+                    case EFAULT: error("problem with copying information from user space");
+                    case EINVAL: error("unexpected period duration");
+                    default: error("unexpected errno value");
+                }
+            }
+        } while(! done);
+    }
+
+    // Avoid useless current drain.
+    if (-1 == gpiod_line_set_value(led, 0)) {
+        error("cannot change the value of the output");
+    }
+
+    gpiod_line_release(led);
+    return NULL;
+}
+
+
+
+int main()
+{
+    struct gpiod_chip *chip;
+    struct led_args   green_args, red_args;
+    pthread_t all_threads[NUMBER_OF_LED];
+    int       all_threads_index = 0;
+
+    // Open GPIO chip
+    chip = gpiod_chip_open_by_name(CHIP_NAME);
+    if (NULL == chip) {
+        error("cannot open the chip");
+    }
+
+    green_args.duration_sec      = 0;
+    green_args.duration_nano_sec = 999999999 / 2; // 1/2 second
+    green_args.count             = 50;
+    green_args.line_id           = GREEN_LED_LINE;
+    green_args.name              = "green";
+    green_args.chip              = chip;
+
+    red_args.duration_sec      = 0;
+    red_args.duration_nano_sec = 999999999 / 3; // 1/3 second
+    red_args.count             = 50;
+    red_args.line_id           = RED_LED_LINE;
+    red_args.name              = "red";
+    red_args.chip              = chip;
+
+    // Start the green LED.
+    if (0 != pthread_create(&all_threads[all_threads_index++], NULL,
+                            &led_thread, (void*)&green_args)) {
+        error("cannot create the thread for the green LED");
+    }
+
+    // Start the green LED.
+    if (0 != pthread_create(&all_threads[all_threads_index++], NULL,
+                            &led_thread, (void*)&red_args)) {
+        error("cannot create the thread for the red LED");
+    }
+
+    // Wait for all threads.
+    for (int i=0; i<all_threads_index; i++) {
+        pthread_join(all_threads[i], NULL);
+    }
+
+    // Release the chip.
+    gpiod_chip_close(chip);
+    return 0;
+}
